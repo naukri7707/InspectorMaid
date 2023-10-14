2b33c72196599ca178c74603c9d2d530abf0d3a1
Naukri <naukri7707@gmail.com>
2023-10-14 17:01:21 +0800

    功能 : 新增 HideDrawer 回傳一個空的裝飾器以隱藏內部所有元素

---
+++ Editor/HideDrawer.cs
@@ -0,0 +1,13 @@
+﻿using Naukri.InspectorMaid.Editor.Core;
+using UnityEngine.UIElements;
+
+namespace Naukri.InspectorMaid.Editor
+{
+    public sealed class HideDrawer : CustomDrawerOf<HideAttribute>
+    {
+        public override VisualElement OnDrawDecorator(VisualElement child, object attribute, DrawerArgs args)
+        {
+            return new VisualElement();
+        }
+    }
+}
