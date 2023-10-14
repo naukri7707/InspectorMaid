69655eb2ab72e7f169a0730f8d41715d39a14fc3
Naukri <naukri7707@gmail.com>
2023-10-14 17:06:16 +0800

    功能 : 新增 ReadOnly 使欄位唯讀

---
+++ Editor/ReadOnlyDrawer.cs
@@ -0,0 +1,23 @@
+﻿using Naukri.InspectorMaid.Editor.Core;
+using UnityEditor.UIElements;
+
+namespace Naukri.InspectorMaid.Editor
+{
+    public class ReadOnlyDrawer : CustomDrawerOf<ReadOnlyAttribute>
+    {
+        public override void OnDrawField(PropertyField field, ReadOnlyAttribute attribute, FieldDrawerArgs args)
+        {
+            field.SetEnabled(false);
+        }
+
+        public override void OnDrawMethod(MethodBuilder builder, ReadOnlyAttribute attribute, MethodDrawerArgs args)
+        {
+            builder.args.Enable = false;
+        }
+
+        public override void OnDrawProperty(PropertyBuilder builder, ReadOnlyAttribute attribute, PropertyDrawerArgs args)
+        {
+            builder.args.Enable = false;
+        }
+    }
+}
