c80485a2446cf1ac6f6488946b05c3681196a367
Naukri <naukri7707@gmail.com>
2023-10-17 17:57:32 +0800

    功能 : ActiveIf Drawer，通過綁定成員動態選擇是否啟用

---
+++ Packages/com.naukri.inspector-maid/Editor/DisableIfDrawer.cs
@@ -0,0 +1,24 @@
+﻿using Naukri.InspectorMaid.Editor.Core;
+
+namespace Naukri.InspectorMaid.Editor
+{
+    public class DisableIfDrawer : CustomDrawerOf<DisableIfAttribute>
+    {
+        public override DecoratorElement OnDrawDecorator(DecoratorElement child)
+        {
+            var decorator = new DecoratorElement("DisableIf Decorator");
+            decorator.OnSceneGUI += Decorator_OnSceneGUI;
+
+            decorator.Add(child);
+
+            return decorator;
+        }
+
+        private void Decorator_OnSceneGUI(DecoratorElement decorator)
+        {
+            var disable = GetBindingValue<bool>();
+
+            decorator.SetEnabled(!disable);
+        }
+    }
+}
---
+++ Packages/com.naukri.inspector-maid/Editor/EnableIfDrawer.cs
@@ -0,0 +1,24 @@
+﻿using Naukri.InspectorMaid.Editor.Core;
+
+namespace Naukri.InspectorMaid.Editor
+{
+    public class EnableIfDrawer : CustomDrawerOf<EnableIfAttribute>
+    {
+        public override DecoratorElement OnDrawDecorator(DecoratorElement child)
+        {
+            var decorator = new DecoratorElement("EnableIf Decorator");
+            decorator.OnSceneGUI += Decorator_OnSceneGUI;
+
+            decorator.Add(child);
+
+            return decorator;
+        }
+
+        private void Decorator_OnSceneGUI(DecoratorElement decorator)
+        {
+            var enable = GetBindingValue<bool>();
+
+            decorator.SetEnabled(enable);
+        }
+    }
+}
