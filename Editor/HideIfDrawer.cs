475f480514ca4f829c90d81b3bb5de7bcf9157d9
Naukri <naukri7707@gmail.com>
2023-10-17 17:58:13 +0800

    功能 : VisibleIf Drawer，通過綁定成員動態選擇是否顯示

---
+++ Editor/HideIfDrawer.cs
@@ -0,0 +1,35 @@
+﻿using Naukri.InspectorMaid.Editor.Core;
+using UnityEngine.UIElements;
+
+namespace Naukri.InspectorMaid.Editor
+{
+    public class HideIfDrawer : CustomDrawerOf<HideIfAttribute>
+    {
+        public override DecoratorElement OnDrawDecorator(DecoratorElement child)
+        {
+            var decorator = new DecoratorElement("HideIf Decorator");
+            decorator.OnSceneGUI += Decorator_OnSceneGUI;
+
+            decorator.Add(child);
+
+            return decorator;
+        }
+
+        private void Decorator_OnSceneGUI(DecoratorElement decorator)
+        {
+            var hide = GetBindingValue<bool>();
+            if (hide == true)
+            {
+                decorator.visible = false;
+                decorator.style.height = 0;
+                decorator.style.width = 0;
+            }
+            else
+            {
+                decorator.visible = true;
+                decorator.style.height = StyleKeyword.Auto;
+                decorator.style.width = StyleKeyword.Auto;
+            }
+        }
+    }
+}
---
+++ Editor/ShowIfDrawer.cs
@@ -0,0 +1,35 @@
+﻿using Naukri.InspectorMaid.Editor.Core;
+using UnityEngine.UIElements;
+
+namespace Naukri.InspectorMaid.Editor
+{
+    public class ShowIfDrawer : CustomDrawerOf<ShowIfAttribute>
+    {
+        public override DecoratorElement OnDrawDecorator(DecoratorElement child)
+        {
+            var decorator = new DecoratorElement("ShowIf Decorator");
+            decorator.OnSceneGUI += Decorator_OnSceneGUI;
+
+            decorator.Add(child);
+
+            return decorator;
+        }
+
+        private void Decorator_OnSceneGUI(DecoratorElement decorator)
+        {
+            var show = GetBindingValue<bool>();
+            if (show == true)
+            {
+                decorator.visible = true;
+                decorator.style.height = StyleKeyword.Auto;
+                decorator.style.width = StyleKeyword.Auto;
+            }
+            else
+            {
+                decorator.visible = false;
+                decorator.style.height = 0;
+                decorator.style.width = 0;
+            }
+        }
+    }
+}
