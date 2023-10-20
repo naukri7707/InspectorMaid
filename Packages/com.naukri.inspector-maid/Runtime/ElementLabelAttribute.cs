4fe8388625c465cbccab6ef712620ed67417e4fc
Naukri <naukri7707@gmail.com>
2023-10-20 20:24:37 +0800

    功能 : LabelDrawer 及 ElementLabelDrawer 以修改欄位及列表元素前綴名稱

---
+++ Packages/com.naukri.inspector-maid/Runtime/ElementLabelAttribute.cs
@@ -0,0 +1,19 @@
+﻿using Naukri.InspectorMaid.Core;
+
+namespace Naukri.InspectorMaid
+{
+    public class ElementLabelAttribute : InspectorMaidBindableAttribute
+    {
+        public ElementLabelAttribute() : this(string.Empty)
+        { }
+
+        public ElementLabelAttribute(string label)
+        {
+            this.label = label;
+        }
+
+        public readonly string label;
+
+        public bool useNicifyName = true;
+    }
+}
---
+++ Packages/com.naukri.inspector-maid/Runtime/LabelAttribute.cs
@@ -0,0 +1,19 @@
+﻿using Naukri.InspectorMaid.Core;
+
+namespace Naukri.InspectorMaid
+{
+    public class LabelAttribute : InspectorMaidBindableAttribute
+    {
+        public LabelAttribute() : this(string.Empty)
+        { }
+
+        public LabelAttribute(string label)
+        {
+            this.label = label;
+        }
+
+        public readonly string label;
+
+        public bool useNicifyName = true;
+    }
+}
