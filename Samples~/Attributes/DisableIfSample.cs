cc17e0bdc8a918741d6b92d408ce8c6d8e3158ae
Naukri <naukri7707@gmail.com>
2023-10-21 00:48:14 +0800

    功能 : 新增各屬性簡單釋例

---
+++ Samples~/Attributes/DisableIfSample.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Naukri.InspectorMaid.Samples
+{
+    public class DisableIfSample : MonoBehaviour
+    {
+        public bool disable;
+
+        [DisableIf(binding = nameof(disable))]
+        public int myField;
+    }
+}
---
+++ Samples~/Attributes/HideIfSample.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Naukri.InspectorMaid.Samples
+{
+    public class HideIfSample : MonoBehaviour
+    {
+        public bool hide;
+
+        [HideIf(binding = nameof(hide))]
+        public int myField;
+    }
+}
