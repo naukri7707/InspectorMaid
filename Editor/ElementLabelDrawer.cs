57217621306f1b82e43614ae4e4e3eb56c9e30e7
Naukri <naukri7707@gmail.com>
2023-10-20 20:24:37 +0800

    功能 : LabelDrawer 及 ElementLabelDrawer 以修改欄位及列表元素前綴名稱

---
+++ Editor/ElementLabelDrawer.cs
@@ -0,0 +1,71 @@
+﻿using Naukri.InspectorMaid.Editor.Core;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+namespace Naukri.InspectorMaid.Editor
+{
+    public class ElementLabelDrawer : CustomDrawerOf<ElementLabelAttribute>
+    {
+        private const string kArrayElementClassName = "unity-base-field__label--with-dragger";
+
+        private List<Label> labelElements;
+
+        private VisualElement memberElement;
+
+        public override void OnDrawField(PropertyField fieldElement)
+        {
+            memberElement = fieldElement;
+        }
+
+        public override void OnDrawMethod(MethodElement methodElement)
+        {
+            memberElement = methodElement;
+        }
+
+        public override void OnDrawProperty(PropertyElement propertyElement)
+        {
+            memberElement = propertyElement;
+        }
+
+        public override void OnSceneGUI()
+        {
+            if (IsBinding && labelElements != null)
+            {
+                var labelText = GetBindingValue<string>();
+
+                for (var i = 0; i < labelElements.Count; i++)
+                {
+                    var labelElement = labelElements[i];
+                    labelElement.text = ActualLabel($"{labelText} {i}");
+                }
+            }
+        }
+
+        public override void OnStart()
+        {
+            if (memberElement != null)
+            {
+                labelElements = memberElement.Query<Label>(className: kArrayElementClassName).ToList();
+            }
+
+            if (!IsBinding && labelElements != null)
+            {
+                var labelText = attribute.label;
+                for (var i = 0; i < labelElements.Count; i++)
+                {
+                    var labelElement = labelElements[i];
+                    labelElement.text = ActualLabel($"{labelText} {i}");
+                }
+            }
+        }
+
+        private string ActualLabel(string labelText)
+        {
+            return attribute.useNicifyName
+                ? ObjectNames.NicifyVariableName(labelText)
+                : labelText;
+        }
+    }
+}
