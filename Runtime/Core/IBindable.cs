02b08ce2eefea6f94a1264130a8924be43201170
Naukri <naukri7707@gmail.com>
2023-10-21 17:07:04 +0800

    重構 : 調整 binding 參數名稱以明確語意

--- Runtime/Core/IBindable.cs
+++ Runtime/Core/IBindable.cs
@@ -5,9 +5,9 @@ namespace Naukri.InspectorMaid.Core
     public interface IBindable
     {
         [SuppressMessage("Style", "IDE1006")]
-        public object[] args { get; set; }
+        public object[] bindingArgs { get; set; }
 
         [SuppressMessage("Style", "IDE1006")]
-        public string binding { get; set; }
+        public string bindingPath { get; set; }
     }
 }
