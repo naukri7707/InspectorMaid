ab0f6102dae1da7438e293e90724e8963249a24f
Naukri <naukri7707@gmail.com>
2023-10-24 13:08:34 +0800

    功能 : 新增 ButtonDrawer 以產生可互動按鈕

---
+++ Runtime/ButtonAttribute.cs
@@ -0,0 +1,31 @@
+﻿using Naukri.InspectorMaid.Core;
+using UnityEngine.UIElements;
+
+namespace Naukri.InspectorMaid
+{
+    public class ButtonAttribute : BindableDrawerAttribute
+    {
+        public ButtonAttribute(
+            string text = "",
+            float width = -1,
+            float height = -1,
+            FlexDirection flexDirection = FlexDirection.ColumnReverse,
+            string binding = null,
+            params object[] args
+            ) : base(binding, args)
+        {
+            this.text = text;
+            this.width = width;
+            this.height = height;
+            this.flexDirection = flexDirection;
+        }
+
+        public readonly string text;
+
+        public readonly float width;
+
+        public readonly float height;
+
+        public readonly FlexDirection flexDirection;
+    }
+}
