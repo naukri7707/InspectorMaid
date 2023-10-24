cee47f5700ef4202b784a58a3a35b39e0520baab
Naukri <naukri7707@gmail.com>
2023-10-24 13:04:23 +0800

    功能 : 新增泛用性較高的 Styler 及其轉換器

---
+++ Packages/com.naukri.inspector-maid/Runtime/Style/AlignSelfAttribute.cs
@@ -0,0 +1,16 @@
+﻿using Naukri.InspectorMaid.Core;
+using UnityEngine.UIElements;
+
+namespace Naukri.InspectorMaid.Style
+{
+
+    public class AlignSelfAttribute : StylerAttribute
+    {
+        public AlignSelfAttribute(Align alignSelf)
+        {
+            this.alignSelf = alignSelf;
+        }
+
+        public readonly StyleEnum<Align> alignSelf;
+    }
+}
---
+++ Packages/com.naukri.inspector-maid/Runtime/Style/BackgroundColorAttribute.cs
@@ -0,0 +1,17 @@
+﻿using Naukri.InspectorMaid.Core;
+using Naukri.InspectorMaid.Converters;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Naukri.InspectorMaid.Style
+{
+    public class BackgroundColorAttribute : StylerAttribute
+    {
+        public BackgroundColorAttribute(string color = null)
+        {
+            backgroundColor = StringConverter.ToStyleColor(color);
+        }
+
+        public readonly StyleColor backgroundColor;
+    }
+}
