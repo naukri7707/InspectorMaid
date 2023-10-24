fbcabc226a2abee5f0cca8dba036cd2895072496
Naukri <naukri7707@gmail.com>
2023-10-24 13:07:56 +0800

    功能 : 新增 HelpBoxDrawer 以產生提示訊息框

---
+++ Packages/com.naukri.inspector-maid/Editor/HelpBoxDrawer.cs
@@ -0,0 +1,32 @@
+﻿using Naukri.InspectorMaid.Editor.Core;
+using Naukri.InspectorMaid.Editor.UIElements;
+using UnityEngine.UIElements;
+
+namespace Naukri.InspectorMaid.Editor
+{
+    public class HelpBoxDrawer : CustomDrawerOf<HelpBoxAttribute>
+    {
+        private HelpBox helpBox;
+
+        public override void OnDrawDecorator(DecoratorElement child)
+        {
+            helpBox = new HelpBox(attribute.message, attribute.messageType);
+
+            decorator.style.flexDirection = attribute.flexDirection;
+            decorator.Add(helpBox);
+
+            child.style.flexGrow = 1;
+
+            base.OnDrawDecorator(child);
+        }
+
+        public override void OnSceneGUI()
+        {
+            if (IsBinding)
+            {
+                var message = GetBindingValue<string>();
+                helpBox.text = message;
+            }
+        }
+    }
+}
