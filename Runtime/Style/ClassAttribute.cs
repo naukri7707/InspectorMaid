5a827a35348a0e77d9f289ebf15a614e216b0f65
Naukri <naukri7707@gmail.com>
2023-10-24 15:14:30 +0800

    功能 : 新增 ClassStyler 以使用 uss 預定義的樣式改變 decorator 樣式

---
+++ Runtime/Style/ClassAttribute.cs
@@ -0,0 +1,14 @@
+﻿using Naukri.InspectorMaid.Core;
+
+namespace Naukri.InspectorMaid.Style
+{
+    public sealed class ClassAttribute : StylerAttribute
+    {
+        public ClassAttribute(params string[] classNames)
+        {
+            this.classNames = classNames;
+        }
+
+        public readonly string[] classNames;
+    }
+}
