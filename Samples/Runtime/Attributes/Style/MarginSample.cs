fc75fb613115f332a490f966f309ff17828ea6c9
Naukri <naukri7707@gmail.com>
2023-10-24 13:10:26 +0800

    功能 : 新增 Styler 範例程式碼

---
+++ Samples/Runtime/Attributes/Style/MarginSample.cs
@@ -0,0 +1,25 @@
+﻿using Naukri.InspectorMaid.Style;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Naukri.InspectorMaid.Samples
+{
+    public class MarginSample : MonoBehaviour
+    {
+        [Margin(all: 10), BackgroundColor("#FF0000"), Container]
+        [BackgroundColor("#0000FF")]
+        public string all;
+
+        [Margin(vertical: 20, horizontal: 10), BackgroundColor("#FF0000"), Container]
+        [BackgroundColor("#0000FF")]
+        public string axis;
+
+        [Margin(bottom: 5), BackgroundColor("#FF0000"), Container]
+        [BackgroundColor("#0000FF")]
+        public string specific;
+
+        [Margin(left: 10, leftUnit: LengthUnit.Percent), BackgroundColor("#FF0000"), Container]
+        [BackgroundColor("#0000FF")]
+        public string withUnit;
+    }
+}
---
+++ Samples/Runtime/Attributes/Style/SizeSample.cs
@@ -0,0 +1,27 @@
+﻿using Naukri.InspectorMaid.Style;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Naukri.InspectorMaid.Samples
+{
+    public class SizeSample : MonoBehaviour
+    {
+        [Size(width: 250)]
+        public string width;
+
+        [Size(height: 30)]
+        public string height;
+
+        [Size(minWidth: 400)]
+        public string minWidth;
+
+        [Size(maxWidth: 450)]
+        public string maxWidth;
+
+        [Size(minHeight: 40)]
+        public string minHeight;
+
+        [Size(maxHeight: 15)]
+        public string maxHeight;
+    }
+}
