5e2b1ba162454ceed74dc0b1d55566e58b063918
Naukri <naukri7707@gmail.com>
2023-10-27 00:38:06 +0800

    重構 : 使用超集合 Style 取代所有 Styler，以減少認知負荷

---
+++ Packages/com.naukri.inspector-maid/Editor/EnableIfScopeDrawer.cs
@@ -0,0 +1,14 @@
+﻿using Naukri.InspectorMaid.Editor.Core;
+
+namespace Naukri.InspectorMaid.Editor
+{
+    public class EnableIfScopeDrawer : CustomDrawerOf<EnableIfScopeAttribute>
+    {
+        public override void OnSceneGUI()
+        {
+            var enable = GetBindingValue<bool>();
+
+            decorator.SetEnabled(enable);
+        }
+    }
+}
--- Packages/com.naukri.inspector-maid/Samples/Attributes/ButtonSample.cs
+++ Packages/com.naukri.inspector-maid/Samples/Attributes/ButtonSample.cs
@@ -1,5 +1,4 @@
-﻿using Naukri.InspectorMaid.Style;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace Naukri.InspectorMaid.Samples.Attributes
@@ -9,8 +8,7 @@ namespace Naukri.InspectorMaid.Samples.Attributes
         [Button("Click me!", binding: nameof(MyMethod))]
         public int myField = 0;
 
-        [Margin(top: 20)]
-        [FlexDirection(FlexDirection.RowReverse), Button("Use FlexDirection to change position!", binding: nameof(MyMethod))]
+        [Button("Use FlexDirection to change position!", binding: nameof(MyMethod))]
         public int myField2 = 0;
 
         public void MyMethod()
