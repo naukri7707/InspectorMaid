70f11be3032edd6163d92140e117ddbcb9f84a6a
Naukri <naukri7707@gmail.com>
2023-10-27 22:39:23 +0800

    功能 : 新增 DividerDrawer 用以分割不同區域的 UI

---
+++ Packages/com.naukri.inspector-maid/Runtime/DividerAttribute.cs
@@ -0,0 +1,14 @@
+﻿using Naukri.InspectorMaid.Core;
+
+namespace Naukri.InspectorMaid
+{
+    public class DividerAttribute : ItemAttribute
+    {
+        public readonly string text;
+
+        public DividerAttribute(string text = null)
+        {
+            this.text = text;
+        }
+    }
+}
