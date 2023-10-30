847e5d67c41faf0bc3455b51a274ecc3b98c6be2
Naukri <naukri7707@gmail.com>
2023-10-30 15:29:07 +0800

    功能 : 新增 CardScope - 帶有輕微圓角和立面陰影的面板

---
+++ Packages/com.naukri.inspector-maid/Editor/CardScopeDrawer.cs
@@ -0,0 +1,30 @@
+﻿using Naukri.InspectorMaid.Editor.Core;
+using Naukri.InspectorMaid.Editor.UIElements;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Naukri.InspectorMaid.Editor
+{
+    public class CardScopeDrawer : CustomDrawerOf<CardScopeAttribute>
+    {
+        public override void OnStart()
+        {
+            var card = attribute.color.HasValue switch
+            {
+                true => new Card(attribute.color.Value.value),
+                false => new Card(),
+            };
+
+            // We need to cache the children before we add the foldout to the decorator,
+            // otherwise the loop will never end.
+            var children = decorator.Children().ToList();
+            foreach (var child in children)
+            {
+                card.hierarchy.Add(child);
+            }
+
+            decorator.Add(card);
+        }
+    }
+}
---
+++ Packages/com.naukri.inspector-maid/Editor/UIElements/Card.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Naukri.InspectorMaid.Editor.UIElements
+{
+    public sealed class Card : VisualElement
+    {
+        public Card() : this(new Color32(56, 56, 56, 255))
+        {
+
+        }
+
+        public Card(Color cardColor)
+        {
+            var outlineColor = new Color(cardColor.r / 2F, cardColor.g / 2F, cardColor.b / 2F, 255);
+            var shadowColor = new Color(cardColor.r / 3F, cardColor.g / 3F, cardColor.b / 3F, 255);
+
+            style.SetMarginSymmetric(vertical: 4);
+            style.SetPaddingSymmetric(vertical: 6, horizontal: 4);
+
+            style.backgroundColor = cardColor;
+            style.SetBorderRadiusAll(4);
+
+            // outline
+            style.SetBorderWidthOnly(top: 1F, left: 1F);
+            style.SetBorderColorOnly(top: outlineColor, left: outlineColor);
+
+            // shadow
+            style.SetBorderWidthOnly(right: 2F, bottom: 2F);
+            style.SetBorderColorOnly(right: shadowColor, bottom: shadowColor);
+        }
+    }
+}
