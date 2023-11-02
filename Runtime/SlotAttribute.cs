5ec30ceb100a8d09c9cfd4e4f94069c87a803a8b
Naukri <naukri7707@gmail.com>
2023-11-03 00:46:44 +0800

    功能 : 新增 SlotWidget，用以插入指定模板使不同欄位可以結合在一起

---
+++ Runtime/SlotAttribute.cs
@@ -0,0 +1,14 @@
+﻿using Naukri.InspectorMaid.Core;
+
+namespace Naukri.InspectorMaid
+{
+    public class SlotAttribute : ItemAttribute
+    {
+        public SlotAttribute(string template)
+        {
+            this.templateName = template;
+        }
+
+        public readonly string templateName;
+    }
+}
