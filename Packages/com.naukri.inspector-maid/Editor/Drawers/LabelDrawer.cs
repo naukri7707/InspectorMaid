0800647c6903b0bb1e13bb3eb7915a6354b0f7bb
Naukri <naukri7707@gmail.com>
2023-11-03 12:25:37 +0800

    修正 : Label 無法正常運作錯誤

--- Packages/com.naukri.inspector-maid/Editor/Drawers/LabelDrawer.cs
+++ Packages/com.naukri.inspector-maid/Editor/Drawers/LabelDrawer.cs
@@ -6,11 +6,28 @@ namespace Naukri.InspectorMaid.Editor.Drawers
 {
     public class LabelDrawer : WidgetDrawerOf<LabelAttribute>, IDrawMemeberReceiver
     {
+        private VisualElement memberElement;
+
         private Label labelElement;
 
         public void OnDrawMember(IWidget widget, VisualElement memberElement)
         {
+            this.memberElement = memberElement;
+        }
+
+        public override void OnStart(IWidget widget)
+        {
+            // we should get labelElement on start,
+            // because labelElement will build while widget attach to panel.
+            // so if we get labelElement on OnDrawMember, it will be null.
             labelElement = memberElement.Q<Label>();
+
+            if (!IsBinding && labelElement != null)
+            {
+                var labelText = attribute.label;
+
+                labelElement.text = ActualLabel(labelText);
+            }
         }
 
         public override void OnSceneGUI(IWidget widget)
@@ -18,16 +35,6 @@ namespace Naukri.InspectorMaid.Editor.Drawers
             if (IsBinding && labelElement != null)
             {
                 var labelText = GetBindingValue<string>();
-
-                labelElement.text = ActualLabel(labelText);
-            }
-        }
-
-        public override void OnStart(IWidget widget)
-        {
-            if (!IsBinding && labelElement != null)
-            {
-                var labelText = attribute.label;
 
                 labelElement.text = ActualLabel(labelText);
             }
