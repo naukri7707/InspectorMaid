4cf619fa52b5de53612996fa65523e86b4813e82
Naukri <naukri7707@gmail.com>
2023-11-05 15:31:44 +0800

    重構 : 調整成員取得邏輯使其在繼承類別也能如預期運作

---
+++ Packages/com.naukri.inspector-maid/Editor/Helpers/InspectorMaidUtility.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+using UnityEngine;
+
+namespace Naukri.InspectorMaid.Editor.Helpers
+{
+    internal static class InspectorMaidUtility
+    {
+        public const BindingFlags kAllAccessFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static;
+
+        public const BindingFlags kAllDeclaredAccessFlags = kAllAccessFlags | BindingFlags.DeclaredOnly;
+
+        public static Type BaseType = typeof(MonoBehaviour); // this should be same as InspectorMaidEditor's CustomEditor attribute
+
+        public static bool HasAttribute<T>(this MemberInfo self) where T : Attribute
+        {
+            return Attribute.IsDefined(self, typeof(T));
+        }
+    }
+}
