688383ea2c4ed13f2b8642f1086cc360aa7a1cc9
Naukri <naukri7707@gmail.com>
2023-11-11 02:45:37 +0800

    功能 : 新增 Spacer 用以填滿容器空隙

---
+++ Packages/com.naukri.inspector-maid/Editor/Widgets/Visual/SpacerWidget.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine.UIElements;
+
+namespace Naukri.InspectorMaid.Editor.Widgets.Visual
+{
+    public class SpacerWidget : ItemWidgetOf<SpacerAttribute>
+    {
+        public override VisualElement Build(IBuildContext context)
+        {
+            var spacer = new VisualElement();
+
+            spacer.style.flexGrow = attribute.flexGrow ?? spacer.style.flexGrow;
+
+            return spacer;
+        }
+    }
+}
