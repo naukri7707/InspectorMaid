ab3c44ccd5612d980b046127802dd300d8128b09
Naukri <naukri7707@gmail.com>
2023-11-11 02:45:37 +0800

    功能 : 新增 Spacer 用以填滿容器空隙

---
+++ Runtime/SpacerAttribute.cs
@@ -0,0 +1,16 @@
+﻿using Naukri.InspectorMaid.Converters;
+using Naukri.InspectorMaid.Core;
+using UnityEngine.UIElements;
+
+namespace Naukri.InspectorMaid
+{
+    public class SpacerAttribute : ItemAttribute
+    {
+        public SpacerAttribute(string flexGrow = "1")
+        {
+            this.flexGrow = StyleStringConverter.ToStyleFloat(flexGrow) ?? this.flexGrow;
+        }
+
+        public readonly StyleFloat? flexGrow;
+    }
+}
