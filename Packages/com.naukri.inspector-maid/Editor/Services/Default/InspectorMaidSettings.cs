d0e8787c08e57a9b59252a7d537335bee9ecf3d0
Naukri <naukri7707@gmail.com>
2023-11-11 00:06:45 +0800

    功能 : 使用 Inspector-Maid 自舉建立 ProjectSetting 頁面

--- Packages/com.naukri.inspector-maid/Editor/Services/Default/InspectorMaidSettings.cs
+++ Packages/com.naukri.inspector-maid/Editor/Services/Default/InspectorMaidSettings.cs
@@ -1,36 +1,32 @@
-﻿using System.IO;
+﻿using Naukri.InspectorMaid.Editor.Core;
+using Naukri.InspectorMaid.Editor.UIElements;
+using Naukri.InspectorMaid.Layout;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
 using UnityEditor;
+using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace Naukri.InspectorMaid.Editor.Services.Default
 {
+    [HideIfScope(nameof(isDrawingSettingsProvider)), ScriptField, EndScope]
+    [Slot(nameof(importStyleSheets))]
     internal partial class InspectorMaidSettings : ScriptableObject, IInspectorMaidSettings
     {
-        [field: SerializeField]
-        public StyleSheet[] ImportStyleSheets { get; set; } = new StyleSheet[0];
+        [SerializeField]
+        private StyleSheet[] importStyleSheets = new StyleSheet[0];
+
+        public StyleSheet[] ImportStyleSheets => importStyleSheets;
     }
 
     partial class InspectorMaidSettings
     {
+        // to tell inspector maid that we are drawing settings provider,
+        // so we will skip the script field at Project Setting Page, but keep it at Inspector.
+        private static bool isDrawingSettingsProvider;
+
         public static InspectorMaidSettings Instance => GetOrCreateSettings();
-
-        [SettingsProvider]
-        internal static SettingsProvider CreateFromSettingsFromFunctor()
-        {
-            var settings = GetOrCreateSettings();
-            var settingsSO = new SerializedObject(settings);
-            var provider = AssetSettingsProvider.CreateProviderFromObject(
-                settingsWindowPath: "Project/Inspector Maid",
-                settingsObj: settings,
-                keywords: new[] { "Inspector Maid", "Inspector", "Maid", "IM" }
-                );
-
-            // Register keywords from the properties
-            provider.keywords = SettingsProvider.GetSearchKeywordsFromSerializedObject(settingsSO);
-
-            return provider;
-        }
 
         private static InspectorMaidSettings GetOrCreateSettings()
         {
@@ -51,32 +47,50 @@ namespace Naukri.InspectorMaid.Editor.Services.Default
             }
             return settings;
         }
-    }
 
-    [CustomEditor(typeof(InspectorMaidSettings))]
-    internal class InspectorMaidSettingsEditor : UnityEditor.Editor
-    {
-        public override void OnInspectorGUI()
+        [SettingsProvider]
+        [SuppressMessage("CodeQuality", "IDE0051")]
+        private static SettingsProvider CreateProvider()
         {
-            using var change = new EditorGUI.ChangeCheckScope();
-
-            var current = serializedObject.GetIterator();
-            if (current.NextVisible(true))
+            var target = Instance;
+            var serializedObject = new SerializedObject(target);
+            IEditorEventService editorEventService = null;
+            var title = "Inspector Maid";
+            var provider = new SettingsProvider($"Project/{title}", SettingsScope.Project)
             {
-                do
+                label = title,
+                keywords = SettingsProvider.GetSearchKeywordsFromSerializedObject(serializedObject),
+                activateHandler = (searchContext, rootElement) =>
                 {
-                    if (current.name == "m_Script")
-                    {
-                        continue;
-                    }
-                    EditorGUILayout.PropertyField(current, true);
-                } while (current.NextVisible(false));
-            }
+                    isDrawingSettingsProvider = true;
+                    var classContext = InspectorMaidEditor.CreateContextTree(target);
+                    var classElement = classContext.Build();
+                    isDrawingSettingsProvider = false;
 
-            if (change.changed)
-            {
-                serializedObject.ApplyModifiedProperties();
-            }
+                    editorEventService = IEditorEventService.Of(classContext);
+                    EditorApplication.update += editorEventService.InvokeUpdate;
+
+                    var page = new ProjectSettingPage(title, classElement);
+                    rootElement.Add(page);
+
+                    // We need to use Bind() after we add classElement to rootElement, otherwise the propertyField in classElement won't show up.
+                    rootElement.Bind(serializedObject);
+                },
+                deactivateHandler = () =>
+                {
+                    if (editorEventService != null)
+                    {
+                        EditorApplication.update -= editorEventService.InvokeUpdate;
+                        editorEventService.InvokeDestroy();
+                    }
+                },
+                inspectorUpdateHandler = () =>
+                {
+                    editorEventService?.InvokeSceneGUI();
+                }
+            };
+
+            return provider;
         }
     }
 }
