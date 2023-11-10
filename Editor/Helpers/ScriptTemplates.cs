115a5b20c6117882afe84d089773d86cc000f50c
Naukri <naukri7707@gmail.com>
2023-11-11 02:49:43 +0800

    功能 : 快速建立特定 CustomEditor ，以幫助使用者啟用 InspectorMaid 服務

---
+++ Editor/Helpers/ScriptTemplates.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+namespace Naukri.InspectorMaid.Editor.Helpers
+{
+    public static class ScriptTemplates
+    {
+        [MenuItem("Assets/Create/Inspector Maid/Custom Editor", false, 79)]
+        public static void CreateCustomEditor()
+        {
+            ScriptFileGenerator.CreateByProjectWidnow("CustomEditor.cs", "CustomEditor.cs");
+        }
+    }
+}
