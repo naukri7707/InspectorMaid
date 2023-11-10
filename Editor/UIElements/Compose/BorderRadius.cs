c0f16ac5d938ceeb3b15436ab57679cac86f8a89
Naukri <naukri7707@gmail.com>
2023-11-11 00:04:58 +0800

    功能 : 新增 UIElements.Compose 使命令式 UI 設計更具結構性

---
+++ Editor/UIElements/Compose/BorderRadius.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine.UIElements;
+
+namespace Naukri.InspectorMaid.Editor.UIElements.Compose
+{
+    public readonly struct BorderRadius
+    {
+        public BorderRadius(StyleLength? topLeft, StyleLength? topRight, StyleLength? bottomLeft, StyleLength? bottomRight)
+        {
+            this.topLeft = topLeft;
+            this.topRight = topRight;
+            this.bottomLeft = bottomLeft;
+            this.bottomRight = bottomRight;
+        }
+
+        public readonly StyleLength? topLeft;
+
+        public readonly StyleLength? topRight;
+
+        public readonly StyleLength? bottomLeft;
+
+        public readonly StyleLength? bottomRight;
+
+        public static BorderRadius All(StyleLength? all = null)
+        {
+            return new BorderRadius(all, all, all, all);
+        }
+
+        public static BorderRadius Vertical(StyleLength? top = null, StyleLength? bottom = null)
+        {
+            return new BorderRadius(top, top, bottom, bottom);
+        }
+
+        public static BorderRadius Horizontal(StyleLength? left = null, StyleLength? right = null)
+        {
+            return new BorderRadius(left, right, left, right);
+        }
+
+        public static BorderRadius Only(StyleLength? topLeft, StyleLength? topRight, StyleLength? bottomLeft, StyleLength? bottomRight)
+        {
+            return new BorderRadius(topLeft, topRight, bottomLeft, bottomRight);
+        }
+    }
+}
---
+++ Editor/UIElements/Compose/BorderSide.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.UIElements;
+
+namespace Naukri.InspectorMaid.Editor.UIElements.Compose
+{
+    public readonly struct BorderSide
+    {
+        public BorderSide(StyleColor? color, StyleFloat? width)
+        {
+            this.color = color;
+            this.width = width;
+        }
+
+        public readonly StyleColor? color;
+
+        public readonly StyleFloat? width;
+
+        public void Deconstruct(out StyleColor? color, out StyleFloat? width)
+        {
+            color = this.color;
+            width = this.width;
+        }
+    }
+}
