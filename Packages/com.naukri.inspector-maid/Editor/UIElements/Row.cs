3840ec5d66f1210576c7a48f5d8ca48509fb6a9f
Naukri <naukri7707@gmail.com>
2023-11-10 11:39:00 +0800

    重構 : 廢棄 ContainerScope 改為使用 ColumnScope 及 RowScope，這能讓語意更明確

---
+++ Packages/com.naukri.inspector-maid/Editor/UIElements/Row.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine.UIElements;
+
+namespace Naukri.InspectorMaid.Editor.UIElements
+{
+    public sealed class Row : VisualElement
+    {
+        public Row(bool reverse = false)
+        {
+            style.flexDirection = reverse switch
+            {
+                true => FlexDirection.RowReverse,
+                false => FlexDirection.Row
+            };
+        }
+    }
+}
--- Packages/com.naukri.inspector-maid/Samples/01. Widget's Attributes/Scripts/EndScopeSample.cs
+++ Packages/com.naukri.inspector-maid/Samples/01. Widget's Attributes/Scripts/EndScopeSample.cs
@@ -4,16 +4,16 @@ namespace Naukri.InspectorMaid.Samples.WidgetAttributes
 {
     public class EndScopeSample : AttributeSampleBehaviour
     {
-        [HelpBox("[EndScope] can close the latest open scope, allowing you to add widgets to another scope.", HelpBoxMessageType.Info)]
+        [HelpBox("[EndScope] can close the lastest open scope, allowing you to add widgets to another scope.", HelpBoxMessageType.Info)]
         [HelpBox("Notice that [EndScope] is a logic widget, so it won't render anything.", HelpBoxMessageType.Warning)]
         [CardScope(color: kSectionBGColor)]
         // Sample 1
         [Target]
-        [ContainerScope, Style(margin: "10 0", padding: "5", backgroundColor: "#404040")]
+        [RowScope, Style(margin: "10 0", padding: "5", backgroundColor: "#606060")]
         [HelpBox("My Element in scope 1", HelpBoxMessageType.Info)]
         [HelpBox("My Element in scope 1", HelpBoxMessageType.Warning)]
         [EndScope]
-        [ContainerScope, Style(margin: "10 0", padding: "5", backgroundColor: "#606060")]
+        [RowScope, Style(margin: "10 0", padding: "5", backgroundColor: "#606060")]
         [HelpBox("My Element in scope 2", HelpBoxMessageType.Error)]
         [EndScope]
         public int Sample1;
