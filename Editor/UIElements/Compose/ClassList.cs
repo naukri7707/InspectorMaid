6ef662a0230fc40aeda3ece0a304e7433a49c818
Naukri <naukri7707@gmail.com>
2023-11-15 15:39:56 +0800

    功能 : 新增 visualElement 相關屬性

---
+++ Editor/UIElements/Compose/ClassList.cs
@@ -0,0 +1,17 @@
+﻿namespace Naukri.InspectorMaid.Editor.UIElements.Compose
+{
+    public readonly struct ClassList
+    {
+        private ClassList(params string[] classNames)
+        {
+            this.classNames = classNames;
+        }
+
+        public readonly string[] classNames;
+
+        public static ClassList Add(params string[] classNames)
+        {
+            return new ClassList(classNames);
+        }
+    }
+}
