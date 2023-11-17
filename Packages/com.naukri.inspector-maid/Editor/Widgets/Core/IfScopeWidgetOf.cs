59db5db1bb9cee33a34ec9211fbe44b0ff2011dd
Naukri <naukri7707@gmail.com>
2023-11-17 13:50:20 +0800

    修正 : IfScope 無法判斷 UObject 問題

--- Packages/com.naukri.inspector-maid/Editor/Widgets/Core/IfScopeWidgetOf.cs
+++ Packages/com.naukri.inspector-maid/Editor/Widgets/Core/IfScopeWidgetOf.cs
@@ -5,14 +5,25 @@ using Naukri.InspectorMaid.Editor.UIElements.Compose;
 using System;
 using System.Linq;
 using UnityEngine.UIElements;
+using UObject = UnityEngine.Object;
 
 namespace Naukri.InspectorMaid.Editor.Widgets.Core
 {
     public abstract class IfScopeWidgetOf<TAttribute> : ScopeWidgetOf<TAttribute>
         where TAttribute : IfScopeAttribute
     {
+        private object bindingMemberDefaultValue;
+
         public sealed override VisualElement Build(IBuildContext context)
         {
+            // Cache the default value for the performance.
+            var bindingValueType = context.GetBindingValueType();
+            bindingMemberDefaultValue = bindingValueType.IsValueType switch
+            {
+                true => Activator.CreateInstance(bindingValueType),
+                false => null,
+            };
+
             var container = CreateContainer().Compose(c =>
             {
                 c.children = BuildChildren(context);
@@ -42,6 +53,28 @@ namespace Naukri.InspectorMaid.Editor.Widgets.Core
             OnUpdateElement(container, condition);
         }
 
+        private bool IsEqual(object lhs, object rhs)
+        {
+            // Because Unity override '==' operator for UObject.
+            // So if any of them is UObject, compare them by '==' operator.
+            if (lhs is UObject || rhs is UObject)
+            {
+                var uLhs = lhs as UObject;
+                var uRhs = rhs as UObject;
+                return uLhs == uRhs;
+            }
+            // Compare them in other case.
+            else
+            {
+                // Cause of the boxing, we can't use '==' operator, otherwise it will compare them by reference.
+                // e.g.
+                // object a = 1;
+                // object b = 1;
+                // bool c = a == b; // c is false
+                return Equals(lhs, rhs);
+            }
+        }
+
         private bool Condition(object bindingValue)
         {
             var res = false;
@@ -50,8 +83,9 @@ namespace Naukri.InspectorMaid.Editor.Widgets.Core
             {
                 res = attribute.values.Length switch
                 {
-                    0 => (bool)bindingValue,
-                    _ => attribute.values.Any(it => bindingValue.Equals(it)),
+                    // return true if value isn't default.
+                    0 => IsEqual(bindingValue, bindingMemberDefaultValue),
+                    _ => attribute.values.Any(it => IsEqual(bindingValue, it)),
                 };
             }
             else if (attribute.conditionLogic == ConditionLogic.Flag)
