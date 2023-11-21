a895294c04623e81e20c30663193b889ba8e1efa
Naukri <naukri7707@gmail.com>
2023-11-21 17:50:27 +0800

    功能 : 新增 OnChanged 用以監聽成員變化，並調用目標函式。

---
+++ Runtime/OnChangedAttribute.cs
@@ -0,0 +1,24 @@
+﻿using Naukri.InspectorMaid.Core;
+
+namespace Naukri.InspectorMaid
+{
+    public class OnChangedAttribute : ItemAttribute, IBindingDataProvider
+    {
+        public OnChangedAttribute(
+            string binding = null,
+            object[] args = null,
+            bool setDirty = false
+            )
+        {
+            this.binding = binding;
+            this.args = args;
+            this.setDirty = setDirty;
+        }
+
+        public bool setDirty;
+
+        public object[] args { get; }
+
+        public string binding { get; }
+    }
+}
