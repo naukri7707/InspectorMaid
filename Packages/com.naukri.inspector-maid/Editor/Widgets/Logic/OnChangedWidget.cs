035ce370006795e7dc5bc1cbd87680e1ea325b16
Naukri <naukri7707@gmail.com>
2023-11-21 17:50:27 +0800

    功能 : 新增 OnChanged 用以監聽成員變化，並調用目標函式。

---
+++ Packages/com.naukri.inspector-maid/Editor/Widgets/Logic/OnChangedWidget.cs
@@ -0,0 +1,43 @@
+﻿using Naukri.InspectorMaid.Editor.Extensions;
+using Naukri.InspectorMaid.Editor.Helpers;
+using Naukri.InspectorMaid.Editor.Services;
+using Naukri.InspectorMaid.Editor.Widgets.Visual;
+using UnityEngine.UIElements;
+
+namespace Naukri.InspectorMaid.Editor.Widgets.Logic
+{
+    public class OnChangedWidget : VisualWidgetOf<OnChangedAttribute>
+    {
+        public override VisualElement Build(IBuildContext context)
+        {
+            var classWidget = ClassWidget.Of(context);
+            var memberWidget = MemberWidget.Of(context);
+
+            var serializedTarget = classWidget.serializedTarget;
+            var memberName = memberWidget.info.Name;
+
+            void onChanged()
+            {
+                if (attribute.setDirty)
+                {
+                    context.RecordAndSetDirty("OnChange Invoked");
+                }
+
+                context.InvokeBindingAction();
+            }
+
+            var previousValue = context.GetValue(memberName);
+
+            context.ListenValue(memberName, obj =>
+            {
+                if (!InspectorMaidUtility.IsBoxedValueEqual(obj, previousValue))
+                {
+                    onChanged();
+                    previousValue = obj;
+                }
+            });
+
+            return null;
+        }
+    }
+}
