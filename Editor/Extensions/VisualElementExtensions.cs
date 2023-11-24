d5b1bcfc618f27197a4d6044d23d0aed792c37bb
Naukri <naukri7707@gmail.com>
2023-11-24 10:42:54 +0800

    功能 : 新增 SetDisplay 作為  VisualElement 的擴充函式

---
+++ Editor/Extensions/VisualElementExtensions.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.UIElements;
+
+namespace Naukri.InspectorMaid.Editor.Extensions
+{
+    public static class VisualElementExtensions
+    {
+        public static void SetDisplay(this VisualElement visualElement, bool display)
+        {
+            visualElement.style.display = display
+                ? DisplayStyle.Flex
+                : DisplayStyle.None;
+        }
+    }
+}
