950c59530f7841188f6eb2702b355f9db8aca5cc
Naukri <naukri7707@gmail.com>
2023-11-24 12:54:29 +0800

    功能 : 新增 GroupScope 可以摺疊的群組

---
+++ Samples/01. Widget's Attributes/Scripts/GroupScopeSample.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine.UIElements;
+
+namespace Naukri.InspectorMaid.Samples.WidgetAttributes
+{
+    [HelpBox("[GroupScope] allows you to create a scope that can be toggled visible by clicking the title.", HelpBoxMessageType.Info)]
+    [Divider("01. Group")]
+    [CardSlot(nameof(sample1))]
+    [Divider("02. Expend by default")]
+    [HelpBox("You can also change the default expand if you want.", HelpBoxMessageType.Info)]
+    [CardSlot(nameof(sample2))]
+    public class GroupScopeSample : AttributeSampleBehaviour
+    {
+        // Sample 1
+        [GroupScope("Help Infos")]
+        [HelpBox("Some help info", HelpBoxMessageType.Info)]
+        [HelpBox("Another help info", HelpBoxMessageType.Info)]
+        [HelpBox("Some warning", HelpBoxMessageType.Warning)]
+        [EndScope]
+        [Target]
+        public int sample1;
+
+        // Sample 2
+        [GroupScope("Help Infos", expend: true)]
+        [HelpBox("Some help info", HelpBoxMessageType.Info)]
+        [HelpBox("Another help info", HelpBoxMessageType.Info)]
+        [HelpBox("Some warning", HelpBoxMessageType.Warning)]
+        [EndScope]
+        [Target]
+        public int sample2;
+    }
+}
