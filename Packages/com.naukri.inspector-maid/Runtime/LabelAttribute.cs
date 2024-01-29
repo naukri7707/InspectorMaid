08c5adf856fcf792ee1a3c92504123041ddc5123
Naukri <naukri7707@gmail.com>
2024-01-29 13:27:33 +0800

    重構 : 新增 Label 用以在 Inspector 上顯示文字

---
+++ Packages/com.naukri.inspector-maid/Runtime/LabelAttribute.cs
@@ -0,0 +1,24 @@
+﻿using Naukri.InspectorMaid.Core;
+
+namespace Naukri.InspectorMaid
+{
+    public class LabelAttribute : ItemAttribute, IBindingDataProvider
+    {
+        public LabelAttribute(
+            string text = "",
+            string binding = null,
+            object[] args = null
+            )
+        {
+            this.text = text;
+            this.binding = binding;
+            this.args = args;
+        }
+
+        public readonly string text;
+
+        public object[] args { get; }
+
+        public string binding { get; }
+    }
+}
