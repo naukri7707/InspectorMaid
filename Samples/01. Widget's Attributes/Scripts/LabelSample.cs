f51451279944098208121b0142f9b0136d04a9cc
Naukri <naukri7707@gmail.com>
2024-01-29 13:27:33 +0800

    重構 : 新增 Label 用以在 Inspector 上顯示文字

---
+++ Samples/01. Widget's Attributes/Scripts/LabelSample.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine.UIElements;
+
+namespace Naukri.InspectorMaid.Samples.WidgetAttributes
+{
+    public partial class LabelSample : AttributeSampleBehaviour
+    {
+        [Label("My Label")]
+        public int simple;
+
+        [Label("My Label", binding: nameof(bindingWithString))]
+        public string bindingWithString = "change me!";
+    }
+
+    [
+        HelpBox("[Label] can show any text on the inspector.", HelpBoxMessageType.Info),
+        // Sample 1
+        GroupScope("01. Label", true),
+            CardSlot(nameof(simple)),
+        EndScope,
+        // Sample 2
+        GroupScope("02. Label with binding", true),
+            HelpBox("[Label] can also use binding to show dynamic text.", HelpBoxMessageType.Info),
+            CardSlot(nameof(bindingWithString)),
+        EndScope,
+    ]
+    partial class LabelSample { }
+}
